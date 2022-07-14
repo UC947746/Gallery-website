4b15ccb5524613df3909fa276cc19427599f51f5
UC947746 <947746@pdsb.net>
2022-07-14 14:49:53 -0400

    sdfsdfdsaf

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+}
